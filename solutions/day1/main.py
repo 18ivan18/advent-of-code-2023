00867803143cb53e77cd3e341d41c12ed08ea6e3
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2023-12-01 13:00:33 +0200

    day1 part1

--- solutions/day1/main.py
+++ solutions/day1/main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from sys import stdin
+import re
 
+def get_digits(x: str):
+    return re.sub('\D+', '', x)
 
 def solve() -> None:
-    input = stdin.read()
+    input = stdin.read().splitlines()
+    print(sum([int(get_digits(x)[0] + get_digits(x)[-1]) for x in input]))
     
 
 
