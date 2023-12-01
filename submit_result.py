99a7c4db93bd61cc8b71a00a4002224d0aa336bf
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2023-12-01 17:19:07 +0200

    update automation

---
+++ submit_result.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+from base import session_cookie, advent_of_code_base_url, year, day
+
+# Submit the solution
+url = f'{advent_of_code_base_url}/{year}/day/{day}/answer'
+data = {'level': 1, 'answer': '123'}
+
+headers = {'cookie': f'session={session_cookie}'}
+
+response = requests.post(url, data=data, headers=headers)
+
+# Print the response from the server
+print(response.text)
--- update_readme.py
+++ update_readme.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
 
 import os
+from base import solutions_dir, day_str
 
-main_dir = 'solutions'
-
-
-def get_latest_day() -> int:
-    days = [int(x[3:]) for x in os.listdir(main_dir)]
-    days.append(0)
-    return sorted(days)[-1]
-
-
-day = get_latest_day()
-day_str = f"day{day}"
-with open(os.path.join(main_dir, day_str, 'README.md')) as f:
+with open(os.path.join(solutions_dir, day_str, 'README.md')) as f:
     problem_name = f.readline()[7:-5]
 
 with open('README.md', 'r+') as readme:
     input = readme.read()
     if problem_name not in input:
-        readme.write(f"- [{problem_name}](./{main_dir}/{day_str})\n")
+        readme.write(f"- [{problem_name}](./{solutions_dir}/{day_str})\n")
     else:
         print(f"{problem_name} already added to README.md")
