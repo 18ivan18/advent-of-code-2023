f2f1c340a10ede8ee24d542a20dc531d358c6d32
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-02 12:11:20 +0200

    fix automation

--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 import os
 import sys
@@ -12,13 +13,8 @@ def today() -> int:
     return sorted(days)[-1]
 
 
-# Opening JSON file
-f = open('.env')
-
-# returns JSON object as
-# a dictionary
-env = json.load(f)
+env = json.load(open('.env' if os.path.exists('.env') else '.env.sample'))
 session_cookie = env['session']
-year = 2023
+year = env['year']
 day = int(sys.argv[1]) if len(sys.argv) > 1 else today()
 day_str = f"{day:02}"
