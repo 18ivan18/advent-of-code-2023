c1fb9c89e4c5fa862391be5616029ff43ceece26
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2023-12-05 15:39:53 +0200

    day 5 part 2 brute force not proud

--- solutions/05/main.py
+++ solutions/05/main.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
-from functools import reduce
 from sys import stdin
 
 
-def create_map(x: str):
+def create_map(x):
     map_values = x.splitlines()[1:]
-    m: list[list[int]] = []
+    m = []
     for x in map_values:
         mapped = list(map(int, x.split()))
         m.append(mapped)
 
     return m
 
+def to_pairs(l):
+    out = []
+    for i in range(0, len(l), 2):
+        out.append((l[i], l[i+1]))
+    return out
 
-def solve() -> None:
+
+def solve():
     input = stdin.read().split('\n\n')
-    seeds = list(map(int, input[0].split(': ')[1].split()))
-    maps: list[list[list[int]]] = []
+    seeds = to_pairs(list(map(int, input[0].split(': ')[1].split())))
+    maps = []
     for x in input[1:]:
         maps.append(create_map(x))
     res = float('inf')
-    for seed in seeds:
-        min_seed = seed
-        for m in maps:
-            for line in m:
-                # destination start range, source range, range length 
-                s, sr, rl = line
-                if sr + rl - 1 >= min_seed and sr <= min_seed:
-                    min_seed = min_seed - sr + s
-                    break
-        res = min(res, min_seed)
+    for start, length in seeds:
+        for seed in range(start, start + length):
+            min_seed = seed
+            for m in maps:
+                for line in m:
+                    # destination start range, source range, range length 
+                    s, sr, rl = line
+                    if sr + rl - 1 >= min_seed and sr <= min_seed:
+                        min_seed = min_seed - sr + s
+                        break
+            res = min(res, min_seed)
     print(res)
 
 
