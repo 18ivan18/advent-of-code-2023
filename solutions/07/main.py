a253b5addaaad311ba7b81e749e465685013fd27
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-07 09:41:26 +0200

    cleanup

--- solutions/07/main.py
+++ solutions/07/main.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from sys import stdin
 from functools import cmp_to_key
 
+
 FIVE_OF_A_KIND = 7
 FOUR_OF_A_KIND = 6
 FULL_HOUSE = 5
@@ -13,13 +14,15 @@ ONE_PAIR = 2
 NONE = 1
 
 
-def hand_type(input: str):
+def hand_type(input: str, wildcard: str = None):
     hand = input.split()[0]
-    if hand == 'JJJJJ':
-        return FIVE_OF_A_KIND
+    if wildcard is not None:
+        if hand == wildcard * 5:
+            return FIVE_OF_A_KIND
+        hand = sorted(hand.replace(wildcard or '', 'a'))
     d = defaultdict(int)
-    for x in sorted(hand.replace('J', 'a')):
-        if x == 'a':
+    for x in hand:
+        if wildcard is not None and x == 'a':
             max_value = max(d, key=d.get)
             d[max_value] += 1
             continue
@@ -47,6 +50,18 @@ def compare(first: str, second: str):
     t1, t2 = hand_type(first), hand_type(second)
     if t1 == t2:
         first = first.replace('A', 'E').replace('K', 'D').replace(
+            'Q', 'C').replace('J', 'B').replace('T', 'A')
+        second = second.replace('A', 'E').replace('K', 'D').replace(
+            'Q', 'C').replace('J', 'B').replace('T', 'A')
+
+        return 1 if first > second else -1
+    return 1 if t1 > t2 else -1
+
+
+def compare_with_wildcard(first: str, second: str):
+    t1, t2 = hand_type(first, wildcard='J'), hand_type(second, wildcard='J')
+    if t1 == t2:
+        first = first.replace('A', 'E').replace('K', 'D').replace(
             'Q', 'C').replace('J', '1').replace('T', 'A')
         second = second.replace('A', 'E').replace('K', 'D').replace(
             'Q', 'C').replace('J', '1').replace('T', 'A')
@@ -58,7 +73,8 @@ def compare(first: str, second: str):
 def solve() -> None:
     input = stdin.read().splitlines()
     input.sort(key=cmp_to_key(compare))
-    print(input)
+    print(sum([int(x.split()[1]) * idx for idx, x in enumerate(input, 1)]))
+    input.sort(key=cmp_to_key(compare_with_wildcard))
     print(sum([int(x.split()[1]) * idx for idx, x in enumerate(input, 1)]))
 
 
