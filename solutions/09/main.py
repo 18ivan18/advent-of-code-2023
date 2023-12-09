009090dedcb8f1b3f78e1311bc10f9d4e9e6567b
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-09 09:40:31 +0200

    day9 part2

--- solutions/09/main.py
+++ solutions/09/main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
+from copy import deepcopy
 from sys import stdin
+
+
+def part1(n_nums):
+    for idx in range(len(n_nums) - 2, -1, -1):
+        n_nums[idx].append(n_nums[idx][-1] + n_nums[idx + 1][-1])
+    return n_nums[0][-1]
+
+
+def part2(n_nums):
+    n_nums[-2].append(n_nums[-2][0])
+    for idx in range(len(n_nums) - 3, -1, -1):
+        n_nums[idx].insert(0, n_nums[idx][0] - n_nums[idx + 1][0])
+    return n_nums[0][0]
 
 
 def solve() -> None:
     input = stdin.read().splitlines()
-    sum = 0
+    sum1, sum2 = 0, 0
     for x in [list(map(int, y.split())) for y in input]:
         n_nums = [x]
         while not all(map(lambda x: x == 0, n_nums[-1])):
@@ -14,10 +28,12 @@ def solve() -> None:
             for idx in range(len(arr) - 1):
                 n_sum.append(arr[idx + 1] - arr[idx])
             n_nums.append(n_sum)
-        for idx in range(len(n_nums) - 2, -1, -1):
-            n_nums[idx].append(n_nums[idx][-1] + n_nums[idx + 1][-1])
-        sum += n_nums[0][-1]
-    print(sum)
+        n_nums1, n_nums2 = n_nums, deepcopy(n_nums)
+        sum1 += part1(n_nums1)
+        sum2 += part2(n_nums2)
+
+    print(sum1)
+    print(sum2)
 
 
 if __name__ == '__main__':
