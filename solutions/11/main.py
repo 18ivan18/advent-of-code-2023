e54f61ea06e7e8586194eb9fd194087e9ee07852
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-11 09:54:16 +0200

    part 2

--- solutions/11/main.py
+++ solutions/11/main.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
 from sys import stdin
-import re
+
+
+def transpose(X):
+    return [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+
+
+expansion_rate1 = 2
+expansion_rate2 = 1_000_000
+
+expanded_cols = set()
+expanded_rows = set()
 
 # (6,1) to (11,5) -> |x1-x2| + |y1-y2| = 9
 # (0,4) to (1,9) -> |x1-x2| + |y1-y2| = 6
 
 
-def shortest_path(x1, y1, x2, y2):
-    return abs(x1-x2) + abs(y1-y2)
-
-
-def transpose(X):
-    return [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+def shortest_path(x1, y1, x2, y2, expansion_rate):
+    # return abs(x1-x2) + abs(y1-y2)
+    min_x, max_x = min(x1, x2), max(x1, x2)
+    min_y, max_y = min(y1, y2), max(y1, y2)
+    path = 0
+    for i in range(min_x, max_x):
+        path += expansion_rate if i in expanded_rows else 1
+    for i in range(min_y, max_y):
+        path += expansion_rate if i in expanded_cols else 1
+    return path
 
 
 def empty(x: str):
@@ -23,25 +37,28 @@ def empty(x: str):
 
 
 def expand(galaxy: list[str]):
-    expanded = []
-    for x in galaxy:
-        expanded.append(x)
-        if empty(x):
-            expanded.append(x)
-    return expanded
+    for i in range(len(galaxy)):
+        if empty(galaxy[i]):
+            expanded_rows.add(i)
+    t = transpose(galaxy)
+    for j in range(len(t)):
+        if empty(t[j]):
+            expanded_cols.add(j)
 
 
 def solve() -> None:
     input = stdin.read().splitlines()
-    expanded = transpose(expand(transpose(expand(input))))
-    sum, galaxies = 0, []
-    for i in range(len(expanded)):
-        for j in range(len(expanded[i])):
-            if expanded[i][j] == '#':
+    expand(input)
+    sum1, sum2, galaxies = 0, 0, []
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            if input[i][j] == '#':
                 for x1, y1 in galaxies:
-                    sum += shortest_path(x1, y1, i, j)
+                    sum1 += shortest_path(x1, y1, i, j, expansion_rate1)
+                    sum2 += shortest_path(x1, y1, i, j, expansion_rate2)
                 galaxies.append((i, j))
-    print(sum)
+    print(sum1)
+    print(sum2)
 
 
 if __name__ == '__main__':
