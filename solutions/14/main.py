2214037054465f88dc731f0988b33646d5758e16
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-14 08:49:19 +0200

    part1

--- solutions/14/main.py
+++ solutions/14/main.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 from sys import stdin
 
 
 def solve() -> None:
-    input = stdin.read().splitlines()
+    input = [list(x) for x in stdin.read().splitlines()]
+    available_slots = defaultdict(list)
+    sum = 0
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            if input[i][j] == '#':
+                available_slots[j] = []
+            if input[i][j] == '.':
+                available_slots[j].append(i)
+            if input[i][j] == 'O' and available_slots[j]:
+                slot = available_slots[j].pop(0)
+                input[i][j], input[slot][j] = input[slot][j], input[i][j]
+                available_slots[j].append(i)
+                sum += len(input) - slot
+            if input[i][j] == 'O':
+                sum += len(input) - i
+
+    print(sum)
 
 
 if __name__ == '__main__':
