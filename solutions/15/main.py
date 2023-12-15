a451f23a5a0bb0e3b1e5a410a60f00048cf0595d
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-15 08:37:11 +0200

    part2

--- solutions/15/main.py
+++ solutions/15/main.py
@@ -22,6 +22,34 @@ def solve() -> None:
     input = stdin.read().split(',')
     print(sum(map(hash, input)))
 
+    boxes = []
+    for _ in range(256):
+        boxes.append(dict())
+
+    for x in input:
+        if '=' in x:
+            label, lens = x.split('=')
+            index = hash(label)
+            box = boxes[index]
+            box[label] = int(lens)
+            continue
+        if '-' in x:
+            label = x[:-1]
+            index = hash(label)
+            box = boxes[index]
+            if label in box:
+                box.pop(label)
+
+    s = 0
+    for idx, x in enumerate(boxes):
+        if len(x) == 0:
+            continue
+        focus = 0
+        for slot, y in enumerate(x.values()):
+            focus += (idx+1)*y*(slot+1)
+        s += focus
+    print(s)
+
 
 if __name__ == '__main__':
     solve()
