028a95612b86503269cf769e53b250e706cb88ba
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-18 12:37:31 +0200

    part2

--- solutions/18/main.py
+++ solutions/18/main.py
@@ -2,35 +2,49 @@
 
 from sys import stdin
 
+digit_to_dir = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
 
-def solve() -> None:
-    input = stdin.read().splitlines()
-    x, y = 0, 0
-    vertices = [(x, y)]
-    border = 0
-    for line in input:
-        direction, length, color = line.split()
-        l = int(length)
-        dx, dy = 0, 0
-        border += l
-        if direction == 'U':
-            dx -= 1
-        if direction == 'D':
-            dx += 1
-        if direction == 'R':
-            dy += 1
-        if direction == 'L':
-            dy -= 1
-        x, y = x+dx*l, y+dy*l
-        vertices.append((x, y))
 
+def shoelace(vertices: list[tuple[int, int]], border: int):
     # something called shoelace formula for finding the area of a polygon
     s1, s2 = 0, 0
     for i in range(len(vertices)-1):
         s1 += vertices[i][0]*vertices[i+1][1]
         s2 += vertices[i][1]*vertices[i+1][0]
     # print(s1, s2, border)
-    print(int(0.5*(abs(s1-s2) + border) + 1))
+    return int(0.5*(abs(s1-s2) + border) + 1)
+
+
+def do_move(vertices: list[tuple[int, int]], direction: str, len: int):
+    x, y = vertices[-1]
+    dx, dy = 0, 0
+    if direction == 'U':
+        dx -= 1
+    if direction == 'D':
+        dx += 1
+    if direction == 'R':
+        dy += 1
+    if direction == 'L':
+        dy -= 1
+    x, y = x+dx*len, y+dy*len
+    vertices.append((x, y))
+
+
+def solve() -> None:
+    input = stdin.read().splitlines()
+    vertices1, vertices2 = [(0, 0)], [(0, 0)]
+    border1, border2 = 0, 0
+    for line in input:
+        direction1, length1, color = line.split()
+        length2 = int(color[2:-2], base=16)
+        direction2 = digit_to_dir[color[-2]]
+        length1 = int(length1)
+        border1 += length1
+        border2 += length2
+        do_move(vertices1, direction1, length1)
+        do_move(vertices2, direction2, length2)
+    print(shoelace(vertices1, border1))
+    print(shoelace(vertices2, border2))
 
 
 if __name__ == '__main__':
