742a9d97f3396123b003ae80763be955814f99b4
Ivan Arabadzhiyski <ajabaja16@gmail.com>
2023-12-27 14:05:48 +0200

    part2 collapse the raph

--- solutions/23/main.py
+++ solutions/23/main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 from sys import stdin, setrecursionlimit
 
 UP, DOWN, LEFT, RIGHT = (-1, 0), (1, 0), (0, -1), (0, 1)
-dirs = {'.': [UP, DOWN, LEFT, RIGHT],
+all_dirs = [UP, DOWN, LEFT, RIGHT]
+dirs = {'.': all_dirs,
         'v': [DOWN], '^': [UP], '>': [RIGHT], '<': [LEFT]}
 
 longest = 0
 visited = set()
+
+# part 2, collapse the graph
+graph = defaultdict(list)
 
 
 def dfs(coords, end, grid, len=1):
@@ -25,6 +30,20 @@ def dfs(coords, end, grid, len=1):
     visited.remove(coords)
 
 
+def dfs2(node, end, len):
+    global longest, longest_path
+    if node == end:
+        longest = max(longest, len)
+        return
+
+    visited.add(node)
+    for next, dist in graph[node]:
+        if next not in visited:
+            dfs2(next, end, len+dist)
+
+    visited.remove(node)
+
+
 def solve() -> None:
     input = stdin.read().splitlines()
     sz = len(input)
@@ -33,6 +52,42 @@ def solve() -> None:
     # lol
     setrecursionlimit(1_000_000)
     dfs((start_x+1, start_y), end, input)
+    print(longest)
+
+    # current coords, start, len
+    s = [((start_x+1, start_y), (start_x, start_y), 1)]
+    while s:
+        curr, start, l = s.pop()
+        if curr == end:
+            graph[start].append((curr, l))
+            graph[curr].append((start, l))
+            continue
+        visited.add(curr)
+        x, y = curr
+        possible = []
+        nodes = []
+        for dx, dy in all_dirs:
+            n_x, n_y = x+dx, y+dy
+            if (n_x, n_y) not in visited and input[n_x][n_y] != '#':
+                possible.append((n_x, n_y))
+            if (n_x, n_y) in graph:
+                nodes.append((n_x, n_y))
+
+        for node in nodes:
+            if l > 1:
+                graph[start].append((node, l+1))
+                graph[node].append((start, l+1))
+
+        if len(possible) > 1:
+            graph[start].append((curr, l))
+            graph[curr].append((start, l))
+        for next in possible:
+            l = 1 if len(possible) > 1 else l + 1
+            start = start if len(possible) == 1 else curr
+            s.append((next, start, l))
+
+    visited.clear()
+    dfs2((start_x, start_y), end, 0)
     print(longest)
 
 
